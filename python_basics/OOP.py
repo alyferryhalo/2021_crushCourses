55b588ed31887c0427405ed2cd882d8f84f9fada
Alisa Gorislav <61160686+alyferryhalo@users.noreply.github.com>
2021-05-25 00:28:43 +0500

    Update OOP.py

--- python_basics/OOP.py
+++ python_basics/OOP.py
@@ -106,6 +106,23 @@ class Zebra():
 
 ###
 
+# Создайте класс Person, у которого есть:
+# конструктор __init__, принимающий 3 аргумента: first_name, last_name, age. 
+# метод full_name, который возвращает строку в виде "<Фамилия> <Имя>"
+# метод is_adult, который возвращает True, если человек достиг 18 лет и False в противном случае;
+
+class Person:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = int(age)
+
+    def full_name(self):
+        return f"{self.last_name} {self.first_name}"
+
+    def is_adult(self):
+        return True if self.age >= 18 else False
 
 ###
 
