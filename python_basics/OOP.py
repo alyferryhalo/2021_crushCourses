6a98385134cf8316be0cd248a7bca77ec8a431d0
Alisa Gorislav <61160686+alyferryhalo@users.noreply.github.com>
2021-05-25 00:13:48 +0500

    Update OOP.py

--- python_basics/OOP.py
+++ python_basics/OOP.py
@@ -17,3 +17,15 @@ class Cat:
 my_cat = Cat()
 
 ###
+
+#  Создайте класс Lion. В нем должен быть метод roar, который печатает на экран "Rrrrrrr!!!"
+# Пример работы с классом Lion
+
+# simba = Lion()
+#simba.roar() # печатает Rrrrrrr!!!
+
+class Lion:
+    def roar(self):
+        print("Rrrrrrr!!!")
+
+###
