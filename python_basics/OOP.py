c70fa257229fa93ff81bcdbe1c13992db2a8b4c3
Alisa Gorislav <61160686+alyferryhalo@users.noreply.github.com>
2021-05-25 00:21:43 +0500

    Update OOP.py

--- python_basics/OOP.py
+++ python_basics/OOP.py
@@ -68,6 +68,21 @@ class Point:
 
 ###
 
+# Создайте класс Laptop, у которого есть:
+# конструктор __init__, принимающий 3 аргумента: brand, model, price 
+# Также во время инициализации необходимо создать атрибут laptop_name - строковое значение, вида "<brand> <model>"
+# И затем создайте 2 экземпляра класса Laptop и сохраните их в переменные laptop1 и laptop2.
+
+class Laptop:
+    
+    def __init__(self,brand,model,price):
+        self.brand = brand
+        self.model = model
+        self.price = price
+        self.laptop_name = f'{brand} {model}'
+        
+laptop1 = Laptop('hp', '15-bw0xx', 57000)
+laptop2 = Laptop('hp', 'pavillion gaming', 100500)
 
 ###
 
