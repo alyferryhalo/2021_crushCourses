228f71872446594660cad6905ad294a1024e05fb
Alisa Gorislav <61160686+alyferryhalo@users.noreply.github.com>
2021-05-25 00:18:49 +0500

    Update OOP.py

--- python_basics/OOP.py
+++ python_basics/OOP.py
@@ -47,3 +47,38 @@ class Counter:
         self.value = 0
         
 ###
+
+# Создайте класс Point. У этого класса должны быть
+# метод set_coordinates, который принимает координаты по x и по y, и сохраняет их в экземпляр класса соответственно в атрибуты x и y 
+# метод get_distance, который обязательно принимает экземпляр класса Point и возвращает расстояние между двумя точками по теореме Пифагора. 
+# В случае, если в данный метод передается не экземпляр класса Point необходимо вывести сообщение "Передана не точка"
+
+class Point:
+
+    def set_coordinates(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_distance(self, instance):
+        if isinstance(instance, Point):
+            return (((instance.x - self.x)**2) + ((self.y - instance.y)**2))**0.5
+        else:
+            print("Передана не точка")
+
+
+###
+
+
+###
+
+
+###
+
+
+###
+
+
+###
+
+
+###
