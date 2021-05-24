554e8ccb5d47b832992b033b356bed0788184d79
Alisa Gorislav <61160686+alyferryhalo@users.noreply.github.com>
2021-05-25 00:31:33 +0500

    Update OOP.py

--- python_basics/OOP.py
+++ python_basics/OOP.py
@@ -126,5 +126,22 @@ class Person:
 
 ###
 
+# Создайте класс Dog, у которого есть:
+# конструктор __init__, принимающий 2 аргумента: name, age. 
+# метод description, который возвращает строку в виде "<name> is <age> years old"
+# метод speak принимающий один аргумент, который возвращает строку вида "<name> says <sound>";
+
+class Dog:
+    
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def description(self):
+        return f"{self.name} is {self.age} years old"
+    
+    def speak(self, sound):
+        self.sound = sound
+        return f"{self.name} says {self.sound}"
 
 ###
