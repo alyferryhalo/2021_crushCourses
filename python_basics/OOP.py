d6c2fe65c5f5c1c1ff75a1f5a5ae61da7149ca62
Alisa Gorislav <61160686+alyferryhalo@users.noreply.github.com>
2021-05-27 19:29:11 +0500

    Update OOP.py

--- python_basics/OOP.py
+++ python_basics/OOP.py
@@ -250,6 +250,54 @@ class UserMail:
     
 ###
 
+# Создайте класс Money, у которого есть:
+
+# конструктор __init__, принимающий 2 аргумента: dollars, cents. По входным аргументам вам необходимо создать атрибут экземпляра total_cents. 
+
+# свойство геттер dollars, которое возвращает количество имеющихся долларов;
+
+# свойство сеттер dollars, которое принимает целое неотрицательное число - количество долларов и устанавливает при помощи него новое значение в атрибут экземпляра total_cents, при этом значение центов должно сохранятся. В случае, если в сеттер передано число, не удовлетворяющее условию, нужно печатать на экран сообщение "Error dollars";
+
+# свойство геттер cents, которое возвращает количество имеющихся центов;
+
+# свойство сеттер cents, которое принимает целое неотрицательное число меньшее 100 - 
+# количество центов и устанавливает при помощи него новое значение в атрибут экземпляра total_cents, при этом значение долларов должно сохранятся. 
+# В случае, если в сеттер передано число, не удовлетворяющее условию, нужно печатать на экран сообщение "Error cents";
+
+#метод __str__ (информация по данному методу), который возвращает строку вида "Ваше состояние составляет {dollars} долларов {cents} центов". 
+# Для нахождения долларов и центов в методе __str__ пользуйтесь свойствами
+
+class Money:
+    
+    def __init__(self, dollars, cents):
+        self.total_cents = dollars * 100 + cents
+
+    @property
+    def dollars(self):
+        return self.total_cents // 100
+
+    @dollars.setter
+    def dollars(self, value):
+        if isinstance(value, int) and value >= 0:
+            self.total_cents = value * 100 + self.total_cents % 100
+        else:
+            print('Error dollars')
+
+    @property
+    def cents(self):
+        return self.total_cents % 100
+
+    @cents.setter
+    def cents(self, value):
+        if isinstance(value, int) and 0 < value < 100:
+            self.total_cents = (self.total_cents - self.total_cents % 100) + value
+        else:
+            print('Error cents')
+
+    def __str__(self):
+        return f'Ваше состояние составляет {self.dollars} долларов ' \
+               f'{self.cents} центов'
+
 ###
 
 # Создайте класс Vector, который хранит в себе вектор целых чисел.  У класса Vector есть:
