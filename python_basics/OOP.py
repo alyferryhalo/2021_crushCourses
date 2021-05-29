952ec44752664a1b012eb373d2915dc9e9ea521a
Alisa Gorislav <61160686+alyferryhalo@users.noreply.github.com>
2021-05-29 13:44:15 +0500

    Update OOP.py

--- python_basics/OOP.py
+++ python_basics/OOP.py
@@ -331,6 +331,37 @@ class Robot:
     @classmethod
     def how_many(cls):
         print(f"{Robot.population}, вот сколько нас еще осталось")   
+        
+###
+
+# Создайте класс Person, у которого есть:
+
+# конструктор __init__, принимающий 3 аргумента: name, surname, gender. 
+# Атрибут gender может принимать только 2 значения: "male" и "female", по умолчанию "male". 
+# Если в атрибут gender передается любое другое значение, печатать сообщение: "Не знаю, что вы имели ввиду? Пусть это будет мальчик!"
+
+# переопределить метод __str__ следующим образом: 
+# если объект - мужчина (атрибут gender = "male"), возвращать строку "Гражданин <Фамилия> <Имя>
+# если объект - женщина (атрибут gender = "female"), возвращать строку "Гражданка <Фамилия> <Имя>
+
+class Person:
+    
+    def __init__ (self, name, surname, gender = 'male'):
+        self.name = name
+        self.surname = surname
+        if gender == 'female':
+            self.gender = 'female'
+        elif gender == '' or gender == 'male':
+            self.gender = 'male'
+        else:
+            print('Не знаю, что вы имели ввиду? Пусть это будет мальчик!')
+            self.gender = 'male'
+
+    def __str__(self):
+        if self.gender == 'male':
+            return f'Гражданин {self.surname} {self.name}'
+        if self.gender == 'female':
+            return f'Гражданка {self.surname} {self.name}'
 
 ###
 
